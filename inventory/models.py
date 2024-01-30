3240c4193a1234699ba73e795b86dd2c173c3871
nikodem <nikodemwilk69@gmail.com>
2024-01-30 01:33:32 +0100

    updating project

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    quantity = models.FloatField(default=0.0)
+    unit = models.CharField(max_length=50)
+    unit_price = models.FloatField(default=0.0)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=500)
+    price = models.FloatField(default=0.0)
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0.0)
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateField(default=datetime.date.today)
